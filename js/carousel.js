c21bf3b69b0f254a2665a8b9f3a3bbbe33d50a4c
lrmaxx <legalraastatech3@gmail.com>
2023-10-16 17:56:20 +0530

    update in services

---
+++ js/carousel.js
@@ -0,0 +1,38 @@
+(function ($) {
+    "use strict";
+
+    // Testimonials carousel
+        $(".testimonials-carousel").owlCarousel({
+            autoplay: true,
+            dots: true,
+            loop: true,
+            responsive: {
+                0:{
+                    items:1
+                },
+                576:{
+                    items:1
+                },
+                768:{
+                    items:2
+                },
+                992:{
+                    items:3
+                }
+            }
+        });
+        
+    
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('filter-active');
+        $(this).addClass('filter-active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+})(jQuery);
